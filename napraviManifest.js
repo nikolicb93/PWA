724ae1ddfcba0c827c4e8c03f43099e93a5d5952
nikolicb93 <nikolicb93@gmail.com>
2022-01-12 10:15:18 +0100

    Inicijalni

---
+++ napraviManifest.js
@@ -0,0 +1,14 @@
+import pwaManifest, { write } from '@pwa/manifest';
+ 
+(async () => {
+    const manifest = await pwaManifest({
+        name: 'Nas prvi PWA',
+        short_name: 'prviPWA',
+        start_url: '/index.html',
+        display: 'standalone',
+        background_color: '#EFEFEF',
+        theme_color: '#FFEEFF'
+    })
+    // dump new generated manifest file if you want
+    await write('./', manifest);
+})();
